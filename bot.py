c8406ed16b696805b9cd681269bacfe42ce9b65f
siraom15 <siriwatjaiyungyuen@gmail.com>
2023-04-26 00:56:16 +0700

    Remove unused comment

--- bot.py
+++ bot.py
@@ -231,46 +231,6 @@ class Bot(commands.Bot):
         except:
             await ctx.send(f"🎶Song not found.")
 
-    # @commands.command(name="skip")
-    # async def skip_song_command(self, ctx):
-    #     sp.next_track()
-    #     await ctx.send(f":) 🎶 Skipping song...")
-
-    # @commands.command(name="albumqueue")
-    #     if ctx.author.is_mod or ctx.author.is_subscriber or self.is_owner(ctx):
-    # async def albumqueue_command(self, ctx, *, album: str):
-    #         album_uri = None
-
-    #         if (
-    #             album.startswith("spotify:album:")
-    #             or not album.startswith("spotify:album:")
-    #             and re.match(URL_REGEX, album)
-    #         ):
-    #             album_uri = album
-    #         await self.album_request(ctx, album_uri)
-    #     else:
-    #         await ctx.send(f"🎶You don't have permission to do that! (Album queue is Sub Only!)")
-
-    """
-        DO NOT USE THE API REQUEST IT WONT WORK.
-        the logic should still work iwth using the spotipy library, so thats why I'm keeping it, but don't do an API request
-        - like this.
-    """
-    # async def album_request(self, ctx, song):
-    #     song = song.replace("spotify:album:", "")
-    #     ALBUM_URL = f"https://api.spotify.com/v1/albums/{song}?market=US"
-    #     async with request("GET", ALBUM_URL, headers={
-    #                 "Content-Type": "application/json",
-    #                 "Authorization": "Bearer " + self.token,
-    #             }) as resp:
-    #             data = await resp.json()
-    #             songs_uris = [artist["uri"] for artist in data['tracks']['items']]
-
-    #             for song_uris in songs_uris:
-    #                 await self.song_request(ctx, song, song_uris, album=True)
-    #             await ctx.send(f"Album Requested! {data['name']}")
-    #             return
-
     async def chat_song_request(self, ctx, song, song_uri, album: bool):
         blacklisted_users = read_json("blacklist_user")["users"]
         if ctx.author.name.lower() in blacklisted_users:
--- update.py
+++ update.py
@@ -1,3 +1,4 @@
 import os
 
 os.system("git pull origin master")
+os.system("pip install -r requirements.txt")
