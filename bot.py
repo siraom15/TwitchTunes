5b634a79ef04745152705fcfd1dfb5e15e34b5e9
siraom15 <siriwatjaiyungyuen@gmail.com>
2023-04-26 00:53:40 +0700

    Update add vip to request song

--- bot.py
+++ bot.py
@@ -22,99 +22,8 @@ logging.basicConfig(
     handlers=[RichHandler()],
 )
 
-
 def path_exists(filename):
     return os.path.join(".", f"{filename}")
-
-
-if not os.path.exists(path_exists("config.json")):
-    print("\n--------------------------")
-    print("\nwhat's your Twitch Bot's name?")
-    bot_name = input(
-        "Type your Bot's name, then press `ENTER`. (You can change this later) "
-    )
-    print("\n--------------------------")
-    print("\nwhat will your prefix be? (example: !, ?, $)")
-    prefix = input("Type your prefix, then press `ENTER`. (You can change this later) ")
-    print("\n--------------------------")
-    print("\nwhat's your Twitch Channel's name?")
-    channel = input(
-        "Type your Channel's name, then press `ENTER`. (You can change this later) "
-    )
-    print("\n--------------------------")
-
-    with open("config.json", "a") as config_file:
-        config_file.write(
-            json.dumps({"nickname": bot_name, "prefix": prefix, "channels": [channel]})
-        )
-
-if not os.path.exists(path_exists(".env")):
-    print("\n⚠⚠⚠⚠⚠ WARNING: DO NOT SHOW THE FOLLOWING ON STREAM. ⚠⚠⚠⚠⚠" * 10)
-    input("\nPress `ENTER` if this is not showing on stream.")
-
-    print("\n" * 100)
-    print("Cool, now let's get to the boring stuff...")
-    print("\n--------------------------")
-    print("\nlet's set up the bot's token")
-    token = input(
-        "You can get this token from a site like https://twitchapps.com/tmi/.\nJust copy and paste the OAuth token into here.\nType token, then press `ENTER`. (You can change this later) "
-    )
-    print("\n--------------------------")
-    print("\nlet's setup the Twitch Client ID")
-    client_id = input(
-        "You can get this by going to https://dev.twitch.tv/console/apps/create, signing in, creating a 'Chat Bot' application (the OAuth redirect URLs NEED to be 'http://localhost:17563/' and 'http://localhost:17563')\nNow just copy and paste the Client ID into here.\nType the Client ID, then press `ENTER`. (You can change this later) "
-    )
-    print("\n--------------------------")
-    print("\nlet's setup the Twitch Client Secret")
-    client_secret = input(
-        'You can get this by scrolling down on your application, and clicking the "New Secret" button.\nNow just copy and paste the Client Secret into here.\nType the Client Secret, then press `ENTER`. (You can change this later) '
-    )
-    print("\n--------------------------")
-    channel_points_reward = input(
-        "If you are going to use TwitchTunes with channel points, then what is your Channel Point reward name?\nType the Channel Point reward name, then press `ENTER`. (You can change this later) "
-    )
-    print("\n--------------------------")
-    print("\nlets setup the Spotify Client ID")
-    spotify_client_id = input(
-        "You can get this by going to https://developer.spotify.com/dashboard/applications, signing in, then creating an application.\nJust paste in the Client ID into here now.\nType Spotify's Client ID, then press `ENTER`. (You can change this later)"
-    )
-    print("\n--------------------------")
-    print("\nlet's setup the Spotify Client Secret")
-    spotify_secret = input(
-        "You can get this by going to that application page, then clicking the 'SHOW CLIENT SECRET' button.\nNow just paste the Client Secret here.\nType Spotify Client Secret, then press `ENTER`. (You can change this later) "
-    )
-    print("\n--------------------------")
-    print("\nlet's setup the Spotify Website/Redirect URI")
-    input(
-        "All you have to do, is hit the settings button, then in BOTH the Website field, AND the Redirect URIs field, but 'http://localhost:8080'\nPress `ENTER` once you have completed this step."
-    )
-
-    with open(".env", "a") as env_file:
-        env_file.write(
-            f"TOKEN={token}\n"
-            + "# Twitch IRC token\n"
-            + f"client_id={client_id}\n"
-            + "# Twitch Client ID from dev.twitch.tv\n"
-            + f"client_secret={client_secret}\n"
-            + "# Twitch Client Secret from dev.twitch.tv\n"
-            + f"channel_points_reward={channel_points_reward}\n"
-            + "# Channel Point reward name\n"
-            + f"\nspotify_client_id={spotify_client_id}\n"
-            + "# Get this from the Spotify console https://developer.spotify.com/dashboard/applications\n"
-            + f"spotify_secret={spotify_secret}\n"
-            + "# Get this from the Spotify console https://developer.spotify.com/dashboard/applications\n"
-            + "spotify_redirect_uri=http://localhost:8080\n"
-            + "# Set your 'redirect_uri' and 'website' on your Spotify application to 'http://localhost:8080' (Don't change the spotify_redirect_uri in .env)"
-        )
-
-if not os.path.exists(path_exists("blacklist.json")):
-    with open("blacklist.json", "a") as blacklist_file:
-        blacklist_file.write(json.dumps({"blacklist": []}))
-
-if not os.path.exists(path_exists("blacklist_user.json")):
-    with open("blacklist_user.json", "a") as blacklist_user_file:
-        blacklist_user_file.write(json.dumps({"users": []}))
-
 
 log.info("\n\nStarting 🎶TwitchTunes")
 
@@ -184,20 +93,6 @@ class Bot(commands.Bot):
 
     def is_owner(self, ctx):
         return ctx.author.id == "640348450"
-
-    # This is an owner only command for an inside joke in a certain channel, just ignore this :)
-    @commands.command(name="s3s")
-    async def s3s(self, ctx):
-        if self.is_owner(ctx) and ctx.channel.name == "tajj":
-            same_3_songs = [
-                "spotify:track:7jVH8CXr0MSpGheHOjN4NA",
-                "spotify:track:0BRbI3ZMPXuj9yA7ChDGOW",
-                "spotify:track:0S8pAna5CIUy0g9XM5hBeF",
-            ]
-            for song in same_3_songs:
-                sp.add_to_queue(song)
-                await asyncio.sleep(0.1)
-            await ctx.send("forsenPls same 3 songs forsenPls")
 
     @commands.command(name="ping", aliases=["ding"])
     async def ping_command(self, ctx):
@@ -291,17 +186,8 @@ class Bot(commands.Bot):
         data = sp.currently_playing()
         song_artists = data["item"]["artists"]
         song_artists_names = [artist["name"] for artist in song_artists]
-
-        min_through = int(data["progress_ms"] / (1000 * 60) % 60)
-        sec_through = int(data["progress_ms"] / (1000) % 60)
-        time_through = f"{min_through} mins, {sec_through} secs"
-
-        min_total = int(data["item"]["duration_ms"] / (1000 * 60) % 60)
-        sec_total = int(data["item"]["duration_ms"] / (1000) % 60)
-        time_total = f"{min_total} mins, {sec_total} secs"
-
         await ctx.send(
-            f"🎶Now Playing - {data['item']['name']} by {', '.join(song_artists_names)} | Link: {data['item']['external_urls']['spotify']} | {time_through} - {time_total}"
+            f"🎶Now Playing - {data['item']['name']} by {', '.join(song_artists_names)}"
         )
 
     @commands.command(
@@ -327,20 +213,23 @@ class Bot(commands.Bot):
     @commands.command(name="songrequest", aliases=["sr", "addsong"])
     async def songrequest_command(self, ctx, *, song: str):
         song_uri = None
-        if ctx.author.is_mod or self.is_owner(ctx):
-            if (
-                song.startswith("spotify:track:")
-                or not song.startswith("spotify:track:")
-                and re.match(URL_REGEX, song)
-            ):
-                song_uri = song
-                await self.chat_song_request(ctx, song_uri, song_uri, album=False)
+        try: 
+            if ctx.author.is_mod or self.is_owner(ctx) or ctx.author.is_vip:
+                if (
+                    song.startswith("spotify:track:")
+                    or not song.startswith("spotify:track:")
+                    and re.match(URL_REGEX, song)
+                ):
+                    song_uri = song
+                    await self.chat_song_request(ctx, song_uri, song_uri, album=False)
 
+                else:
+                    await self.chat_song_request(ctx, song, song_uri, album=False)
             else:
-                await self.chat_song_request(ctx, song, song_uri, album=False)
-        else:
-            await ctx.send(f"@{ctx.author.name}, you don't have permission to do that.")
-            return
+                await ctx.send(f"@{ctx.author.name} you should be a VIP, Mod to use this command.")
+                return
+        except:
+            await ctx.send(f"🎶Song not found.")
 
     # @commands.command(name="skip")
     # async def skip_song_command(self, ctx):
